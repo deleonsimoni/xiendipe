578ce7b9a3d645b39344851fd340fa878ae39445
Deleon Simoni <deleon.simoni@gmail.com>
2022-03-16 20:22:27 -0300

    desabilitar recursos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   //host: 'http://xxendiperio2020.com.br:8080/api'
   //HOMOLOGACAO
-  host: 'http://15.229.13.3:8080/api'
+  //host: 'http://15.229.13.3:8080/api'
   //PROD
-  //host: 'http://18.231.22.60:8080/api'
+  host: 'http://xxiendipe.com.br:8080/api'
 };
