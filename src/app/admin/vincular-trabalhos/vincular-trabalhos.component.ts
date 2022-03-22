ebb26976ea5810e08bf898bac14d35fa1c3d3c8f
TATIANA STOFELLA <tatiana_sodre@yahoo.com.br>
2022-03-21 23:11:01 -0300

    eixo

--- src/app/admin/vincular-trabalhos/vincular-trabalhos.component.ts
+++ src/app/admin/vincular-trabalhos/vincular-trabalhos.component.ts
@@ -14,15 +14,15 @@ import { TypeWorkRelatorioPipe } from "src/app/pipes/type-work-relatorio.pipe";
 })
 export class VincularTrabalhosComponent implements OnInit {
   public eixos = [
-    { id: 1, name: "A Didática como campo epistemológico e disciplinar" },
-    { id: 2, name: "A Didática e os Saberes docentes estruturantes na formação de professores" },
-    { id: 3, name: "A Didática e as tecnologias da informação e comunicação no currículo e práticas de ensino" },
-    { id: 4, name: "A Didática e Práticas de Ensino na perspectiva da Educação como Direito Constitucional e os desafios políticos da atualidade" },
-    { id: 5, name: "A Didática e as Práticas de ensino nas políticas de formação de Pedagogos(as)" },
-    { id: 6, name: "A Didática e as Práticas de Ensino nos cursos de Licenciatura: entre tensionamentos e perspectivas" },
-    { id: 7, name: "A Didática, Práticas de Ensino - Infâncias, Juventudes e Vida Adulta" },
-    { id: 8, name: "A Didática, Práticas de Ensino, Educação das Relações Étnico-raciais, Diversidade e Inclusão Escolar" },
-    { id: 9, name: "A Didática da Educação Superior" },
+    { id: 1, name: "Eixo 1: A Didática como campo epistemológico e disciplinar" },
+    { id: 2, name: "Eixo 2: A Didática e os Saberes docentes estruturantes na formação de professores" },
+    { id: 3, name: "Eixo 3: A Didática e as tecnologias da informação e comunicação no currículo e práticas de ensino" },
+    { id: 4, name: "Eixo 4: A Didática e Práticas de Ensino na perspectiva da Educação como Direito Constitucional e os desafios políticos da atualidade" },
+    { id: 5, name: "Eixo 5: A Didática e as Práticas de ensino nas políticas de formação de Pedagogos(as)" },
+    { id: 6, name: "Eixo 6: A Didática e as Práticas de Ensino nos cursos de Licenciatura: entre tensionamentos e perspectivas" },
+    { id: 7, name: "Eixo 7: A Didática, Práticas de Ensino - Infâncias, Juventudes e Vida Adulta" },
+    { id: 8, name: "Eixo 8: A Didática, Práticas de Ensino, Educação das Relações Étnico-raciais, Diversidade e Inclusão Escolar" },
+    { id: 9, name: "Eixo 9: A Didática da Educação Superior" },
   ];
   public axisId = null;
   public works = [];
