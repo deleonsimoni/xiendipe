4d7359512518799b91e61558ad22c6a8cd05816a
TATIANA STOFELLA <tatiana_sodre@yahoo.com.br>
2022-05-05 22:30:19 -0300

    emails e prazos e menus

--- server/utils/emailsGroup1.js
+++ server/utils/emailsGroup1.js
@@ -1,10 +1,16 @@
 module.exports = Object.freeze({
 
-        EMAILS_GROUP_1: [
-            'tatiana_sodre@yahoo.com.br',            
-            'deleon.simoni@gmail.com',
-            'edukonlinebr@gmail.com',
-            'tatiana.sodre@gmail.com'
-        ],
-  
-    });
+    EMAILS_GROUP_1: [
+        'tatiana_sodre@yahoo.com.br',
+        'deleon.simoni@gmail.com',
+        'edukonlinebr@gmail.com',
+        'tatiana.sodre@gmail.com',
+        'camilima8@gmail.com',
+        'priscilla_andrade@ufcat.edu.br',
+        'lucianaguimaraespedro@gmail.com',
+        'giovannabernini@hotmail.com',
+        'nagillavieira@gmail.com',
+        'naiara_vilela@hotmail.com'
+    ],
+
+});
